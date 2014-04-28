a30bf88a8d9b5b966f90ea368575a9053c51eda9
Zach Moore <zcmoore@asu.edu>
2014-04-28 14:28:32 -0700

    itemized tests

--- tests/tests/TestRandomPlayer.java
+++ tests/tests/TestRandomPlayer.java
@@ -6,12 +6,20 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 import connect.four.board.Board;
+import connect.four.board.ColumnFullException;
 import connect.four.player.RandomPlayer;
 
 public class TestRandomPlayer
 {
 	@Test
-	public void test()
+	public void testName()
+	{
+		RandomPlayer randomPlayer = new RandomPlayer();
+		assertEquals("Computer", randomPlayer.getName());
+	}
+	
+	@Test
+	public void testPerformPlay()
 	{
 		final int boardWidth = 5;
 		final int boardHeight = 5;
@@ -37,6 +45,38 @@ public class TestRandomPlayer
 			e.printStackTrace();
 			fail("exception in board");
 		}
+	}
+	
+	@Test
+	public void testInvalidPlay()
+	{
+		final int boardWidth = 5;
+		final int boardHeight = 5;
+		Board board = new Board(boardWidth, boardHeight);
+		RandomPlayer randomPlayer = new RandomPlayer();
+		
+		try
+		{
+			for (int i = 0; i < boardHeight * boardWidth; i++)
+			{
+				randomPlayer.performPlay(board);
+			}
+			
+			// Make a play on a full board - catch exception below
+			randomPlayer.performPlay(board);
+			fail("Expect ColumnFullException to be thrown");
+		}
+		catch (ColumnFullException e)
+		{
+			assertEquals("Expect full board", boardHeight * boardWidth,
+					board.getMoveCount());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			fail("exception in board");
+		}
+		
 	}
 	
 }
